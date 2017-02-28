9e82dcbda49f581e062e475ee1dc83d36f927732
César Ramírez <cesar.ramirez.sv@gmail.com>
2017-02-28 13:04:28 -0600

    Added components design Sidebar, Pad and Note

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <sidebar></sidebar>
+    <pad></pad>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import Pad from './components/Pad'
+import Sidebar from './components/Sidebar'
 
 export default {
   name: 'app',
   components: {
-    Hello
+    Pad,
+    Sidebar
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    body, html {
+        margin: 0;
+        padding: 0;
+        height: 100%;
+    }
+
+    body {
+        color: #2c3e50;
+    }
+
+    #app {
+      font-family: 'Raleway', Helvetica, Arial, sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      display: flex;
+      height: 100%;
+    }
 </style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,43 @@
+<template lang="html">
+    <div class="sidebar">
+        <note></note>
+        <note></note>
+        <note></note>
+        <!-- <div class="sidebar__content">
+            <a href="#">Start new note!</a>
+        </div>
+        <div class="sidebar__content">
+            Boo, no notes. Get writing!
+        </div> -->
+    </div>
+</template>
+
+<script>
+import Note from './Note'
+
+export default {
+  components: {
+    Note
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+    .sidebar {
+        background-color: #f9f9f9;
+        flex: 1;
+        height: 100%;
+        min-width: 28%;
+        border-right: 2px solid #eee;
+        // overflow-y: scroll;
+
+        &__content {
+            padding: 30px;
+
+            a {
+                text-decoration: none;
+                color: inherit;
+            }
+        }
+    }
+</style>
